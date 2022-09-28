132eef8842ae15c84e95d0465f100967864eb53b
Paris <paris.140815@gmail.com>
2022-09-27 22:35:30 -0500

    API use with fetch JS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react"
+
 const App = () => {
+  const apiURL = import.meta.env.VITE_API_URL
+  const [cryptos, setCryptos] = useState()
+  useEffect(()=>{
+    fetch(`${apiURL}assets`)
+    .then((fetchedInfo) => fetchedInfo.json())
+    .then((datJson) => {
+      setCryptos(datJson.data)
+    })
+    .catch((err) => {
+      console.error(err.message)
+    })
+  }, [])
+
+  if (!cryptos) return(<div>Loading... please wait</div>)
+
   return(
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <ol>
+        {
+          cryptos.map(({id, name, priceUsd}) => (
+            <li key={id}>{name}   ${priceUsd}</li>
+          ))
+        }
+      </ol>
+    </>
   )
 }
 
